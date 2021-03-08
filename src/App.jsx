27175e892f7b3a6c85a7b5bce2bdf6df5f7dc3df
Travis Welch <trav.cwelch@gmail.com>
2021-03-08 15:43:19 -0800

    add sessions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 
@@ -7,60 +7,46 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import AccountPage from './pages/AccountPage'
 import Header from './components/Header'
+import { SessionContext, getSessionCookie, setSessionCookie } from "./components/session.ts";
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.setcredentials = this.setcredentials.bind(this)
     this.state = {
       title: "Metrics",
       subtitle : "quantify your life",
       login : {
         title: "metrics Login"
-      },
-      user : {
-        loggedIn: false,
-        username: "",
-        email: "",
-        password: "",
-        key: "",
-        refresh : ""
       }
     }
   }
 
-  setcredentials(props) {
-    this.setState({
-      user:{
-        loggedIn: false,
-        username: props.username,
-        email: props.email,
-        password: props.password,
-        key: props.accessKey,
-        refresh : props.refreshKey
-      }
-    })
-    console.log("Entered credentials function")
-  }
-
   render() {
+    const [session, setSession] = useState(getSessionCookie());
+    useEffect(
+      () => {
+        setSession(getSessionCookie);
+      }
+    )
     return (
-      <Router >
-        <Header />
-        <Container>
-          <Route path="/" exact render={() => <LandingPage />} />
-          <Route exact path="/login">
-            {this.state.user.loggedIn ? <Redirect
-              to={{
-                pathname: "/dashboard/:userName",
-                state: this.state.user
-              }}
-            /> : <LoginPage onClick={this.setcredentials}/>}
-          </Route>
-          <Route path="/register" render={() => <RegisterPage />} />
-        </Container>
-      </Router>
+      <SessionContext.Provider value={session}>
+        <Router >
+          <Header />
+          <Container>
+            <Route path="/" exact render={() => <LandingPage />} />
+            <Route exact path="/login">
+              {this.state.user.loggedIn ? <Redirect
+                to={{
+                  pathname: "/dashboard/:userName",
+                  state: this.state.user
+                }}
+              /> : <LoginPage onClick={this.setcredentials}/>}
+            </Route>
+            <Route path="/register" render={() => <RegisterPage />} />
+          </Container>
+        </Router>
+      </SessionContext.Provider>
     )
   }
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { setSessionCookie, getSessionCookie } from "../components/session.ts"
 import "../styles/form.css";
 
 function Login(props, { onClick }) {
@@ -8,14 +9,19 @@ function Login(props, { onClick }) {
   const [password, setPassword] = useState("");
   const [accessKey, setAccessKey] = useState("");
   const [refreshKey, setRefreshKey] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = usState(false);
+
 
   function validateForm() {
     //console.log("validated!")
     return username.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event, props) {
+  function handleSubmit() {
     event.preventDefault();
+    setLoading(true);
+
     fetch('http://localhost:8000/auth/login/', {
      method: 'post',
      headers: {'Content-Type':'application/json'},
@@ -26,16 +32,15 @@ function Login(props, { onClick }) {
     })
     .then((response) => response.json())
      .then((responseJson) => {
-       setAccessKey(responseJson.access)
-       setRefreshKey(responseJson.refresh)
-       //props.onClick(username, password)
+       setAccessKey(responseJson.access);
+       setRefreshKey(responseJson.refresh);
      })
      .catch((error) => {
        console.error(error);
      });
-  }
 
-  function sendCredentials(){
+     setSessionCookie({ username, password, accessKey, refreshKey })
+     setLoading(false);
 
   }
 
